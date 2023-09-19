5d02121c0f4dc69c738e73b76c356b8dd3f73db5
Moksh1201 <130505679+Moksh1201@users.noreply.github.com>
2023-09-20 03:41:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,47 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mailgun = require('mailgun-js');
+import './App.css';
+import
+function App() {
+  const [selectedOption, setSelectedOption] = useState('Question'); // Default to 'Question'
 
-const api_key = 'key-46f40d334023c89603612dcccb495f66'; 
-const domain = 'sandbox5f61c96513ff4c5a9f37c034be2df31d.mailgun.org'; 
-const mailgunInstance = mailgun({ apiKey: api_key, domain: domain });
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+  // Define description text based on the selected option
+  const descriptionText = selectedOption === 'Question' ? 'Describe your problem' : 'Article text';
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+  return (
+    <div>
 
-app.post('/', (req, res) => {
-    const email = req.body.email;
+      
+    </div>
+  );
+}
 
-    const mailData = {
-        from: 'Moksh <moksh4814.be22@chitkara.edu.in>',
-        to: 'moksh4814.be22@chitkara.edu.in',
-        subject: 'Hello',
-        text: 'Testing completed by Moksh 2210994814',
-    };
-
-    mailgunInstance.messages().send(mailData, function (error, body) {
-        if (error) {
-            console.log(error);
-            return res.status(500).send("Error sending email");
-        } else {
-            console.log(body);
-            res.sendFile(__dirname + '/index.html');
-        }
-    });
-});
-
-<div id="root"></div>
-
-
-app.listen(5502, () => {
-    console.log("The Server is running at port 5502!");
-});
-
-export default app
-    
-
+export default App;
