e40926d0ef4d43490a74bea5e5da957acdddf866
Moksh1201 <130505679+Moksh1201@users.noreply.github.com>
2023-09-20 03:41:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Bar from './Topnav'
+import Image from './headerimg'
+import Articles from './Articletemplate'
+import Tutorials from './Tutorialstemplate';
+import Foot from './lastnav';
+import Email from './Email';
 
-function Card(props){
-    return(
-      <div className="article">
-      <img src={props.imgSrc} alt={articleTi} />
-      <div className="article-title">{props.articleTitle}</div>
-      <div className="article-description">{props.articleDescription}</div>
-      <div className="article-rating">&#11088; Rating: {props.articleRating}</div>
-      <div className="article-author">Author: {props.articleAuthor}</div>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <Bar/>
+    <Image/>
+    <Articles/>
+    <br></br>
     
-
-
-const root = ReactDOM.createRoot();
-root.render( <>
-<Card imgsrc='art1.jpg'
-articleTitle ='Article 1' 
-articleDescription = 'Description of Article 1' 
-articleRating= '5' articleAuthor='NAME'/>
-
-<Card imgsrc='art1.jpg'
-articleTitle ='Article 1' 
-articleDescription = 'Description of Article 2' 
-articleRating= '4' articleAuthor='NAME'/>
-
-<Card imgsrc='art1.jpg'
-articleTitle ='Article 1' 
-articleDescription = 'Description of Article 3' 
-articleRating= '4.3' articleAuthor='NAME'/>
-
-<button>See all articles</button>
-
-<Card imgsrc='tutorial.jpg'
-articleTitle ='Article 1' 
-articleDescription = 'Description of description 1' 
-articleRating= '5' articleAuthor='NAME'/>
-
-<Card imgsrc='tutorial.jpg'
-articleTitle ='Article 1' 
-articleDescription = 'Description of description 2' 
-articleRating= '4.4' articleAuthor='NAME'/>
-
-<Card imgsrc='tutorial.jpg'
-articleTitle ='Article 1' 
-articleDescription = 'Description of description 3' 
-articleRating= '3.2' articleAuthor='NAME'/>
-
-<button>See all tutorials</button>
-</>
-
-,document.getElementById('root')
+    <Tutorials/>
+    <br></br>
+    <br></br>
+    <Email/>
+    <br></br>
+    <Foot/>
+    
+  </div>
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
